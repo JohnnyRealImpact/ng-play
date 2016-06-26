7282fd9ffd6e8e1d308f73706f639f0a054c3ab6
JohnathanLodder <johnathan.lodder@realimpactanalytics.com>
2016-06-26 20:27:00 +0200

    Tweaks to the main UI and refactored some code

--- client/app/app.js
+++ client/app/app.js
@@ -75,14 +75,214 @@ angular.module('ngPlayApp', [
       });
   })
 
+  .constant('appConfig', {
+    api: {
+      things: '/api/things',
+    },
+    cmd: {
+      discoverIp: 'dig +short myip.opendns.com @resolver1.opendns.com',
+      dockerMachineVer: 'docker-machine -v | sed -e "s/docker-machine version //g" | sed -e "s/, build .*$//g" | sed -e "s/ //g"',
+      dockerMachineList: 'printf "[ " && docker-machine ls --format="{ \\\"name\\\": \\\"{{.Name}}\\\", \\\"state\\\": \\\"{{.State}}\\\", \\\"driver\\\": \\\"{{.DriverName}}\\\", \\\"url\\\": \\\"{{.URL}}\\\" }," && printf " null ]\n" | sed -e "s/\\},\\s*\\]/zzzz/g"',
+    }
+  })
+  .service('appRuntime', ['appConfig', 'cliProxy', '$http', function (config, cliProxy, $http) {
+    // Ensure list exists
+
+    var _self = this;
+    _self.awesomeThings = _self.awesomeThings || [];
+    _self.init = function () {
+      // Check for auto discovery
+      if (config.api && config.api.things) {
+        $http
+          .get(config.api.things)
+          .success(function (awesomeThings) {
+            _self.awesomeThings.concat(awesomeThings);
+          })
+      }
+    };
+    _self.getAwesomeThings = function(){
+      return _self.awesomeThings;
+    };
+    _self.deleteAwesomeThing = function (thing) {
+      $http.delete(config.api.things + '/' + thing._id);
+    };
+    _self.checkLocal = function () {
+      var localTile = {
+        name: 'Remote Host (checking...)',
+        desc: 'Remotely installed environment',
+        url: 'https://localhost',
+        active: false,
+        featured: false,
+      };
+      _self.awesomeThings.push(localTile);
+
+      // Resolve public IP address (via bash on server)
+      return cliProxy
+        .send({
+          input: config.cmd.discoverIp,
+        })
+        .then(function (resp) {
+          if (resp.stderr) {
+            // Update placeholder tile
+            localTile.name = 'Remote Host (offline)';
+            localTile.info = 'An error occurred while looking for docker-machine';
+            console.error(resp.stderr);
+          } else if (resp.stdout) {
+            localTile.name = 'Remote Host';
+            localTile.url = 'https://' + resp.stdout;
+            localTile.active = true;
+          }
+        });
+
+
+      //return $http
+      //  .get(localTile.url)
+      //  .success(function (data) {
+      //    localTile.name = 'localhost (secure)';
+      //    localTile.url = 'https://localhost';
+      //    localTile.active = true;
+      //    _self.awesomeThings.push(localTile);
+      //  })
+      //  .catch(function (err) {
+      //    console.warn('Not found: ' + localTile.url);
+      //
+      //    localTile.url = 'http://localhost';
+      //    localTile.name = 'localhost (insecure)';
+      //    localTile.active = false;
+      //
+      //    return $http
+      //      .get(localTile.url)
+      //      .success(function (data) {
+      //        console.info('Connected: ' + localTile.url);
+      //        localTile.active = true;
+      //        _self.awesomeThings.push(localTile);
+      //      })
+      //      .catch(function (err) {
+      //        localTile.url = 'http://localhost';
+      //        localTile.name = 'localhost (offline)';
+      //        localTile.active = false;
+      //        localTile.error = err;
+      //        console.warn('Not Found: ' + localTile.url, err);
+      //      })
+      //  })
+    };
+    _self.checkDockers = function() {
+      // Send the command to the remote machine
+      var tileDockerMachine = {
+        name: 'docker-machine',
+        desc: 'Checking if locally installed...',
+        active: false,
+      };
+      _self.awesomeThings.push(tileDockerMachine);
+
+
+      return cliProxy
+        .send({
+          input: config.cmd.dockerMachineVer
+        })
+        .then(function (resp) {
+          if (resp.stderr) {
+            // Update placeholder tile
+            tileDockerMachine.name = 'docker-machine (not found)';
+            tileDockerMachine.info = 'An error occurred while looking for docker-machine';
+            console.error(resp.stderr);
+          } else {
+            // Update placeholder tile
+            tileDockerMachine.name = 'docker-machine (' + resp.stdout.trim() + ')';
+            tileDockerMachine.desc = 'Successfully located docker-machine on the host.';
+
+            return cliProxy
+              .send({
+                input: config.cmd.dockerMachineList
+              })
+              .then(function (resp) {
+                if (resp.stderr) {
+                  // Update placeholder tile
+                  tileDockerMachine.error = new Error(resp.stderr);
+                  tileDockerMachine.name = 'docker-machine (error)';
+                  tileDockerMachine.info = 'An error occurred while looking for docker-machine';
+
+                  console.error(resp.stderr);
+                } else {
+
+                  // Remove placeholder tile
+                  var index = _self.awesomeThings.indexOf(tileDockerMachine);
+                  if (index > -1) {
+                    _self.awesomeThings.splice(index, 1);
+                  }
+
+                  // { state: Running, driver: virtualbox, name:default, url: tcp://192.168.99.100:2376 }
+                  var found = {};
+                  var result = JSON.parse(resp.stdout);
+                  if (result.length) {
+                    result.forEach(function (item) {
+                      if (!item) return;
+                      found[item.name] = item;
+
+                      console.log(' - Adding:', item);
+                      var card = {
+                        icon: 'hardware:developer_board',
+                        name: item.name,
+                        desc: item.driver + ', Url: ' + item.url,
+                        info: {},
+                        active: true,
+                      };
+                      _self.awesomeThings.push(card);
+
+                      cliProxy
+                        .send({
+                          input: 'docker-machine inspect ' + item.name,
+                        })
+                        .then(function (resp) {
+                          if (resp.stderr) {
+                            console.warn(resp.stderr);
+                          }
+                          if (resp.stdout) {
+                            card.info = JSON.parse(resp.stdout);
+                            card.url = 'https://' + card.info.Driver.IPAddress + '/';
+                            //console.log(' - Updated: ', card.info);
+                          }
+                        });
+
+
+                    });
+                  }
+
+                  _self.awesomeThings.push(
+                    {
+                      icon: 'content:add',
+                      name: 'Create New',
+                      info: 'Define a new docker-machine environment.',
+                      active: false,
+                    }
+                  );
+
+                }
+              })
+          }
+        })
+        .catch(function (err) {
+          console.warn(' - REJ:', err);
+        })
+    };
+
+  }])
+
   .filter('fromNow', function() {
     return function(dateString, format) {
       return moment(dateString).fromNow(format);
     };
   })
 
-  .run(function($state, $timeout) {
+  .run(function($state, $timeout, appRuntime) {
     console.log('Starting application...');
+
+    appRuntime.init();
+    appRuntime.checkLocal()
+      .then(function(res) {
+        return appRuntime.checkDockers()
+      })
+      //.finally($rootScope.$applyAsync);
 
     // Hacky way to set initial state
     if ($state && $state.current && !$state.current.name) {
